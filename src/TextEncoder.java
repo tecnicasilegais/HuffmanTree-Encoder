01157d20395a1159c5646acb0d3047ac39664292
ITWhiteRaccoon <eduardo.a@edu.pucrs.br>
2019-09-24 02:18:33 -0300

    Frequency Map
    -Added method to create a character frequency map from a string

--- src/TextEncoder.java
+++ src/TextEncoder.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /**
  * Implementation of a text encoder using Huffman Tree
  * 
@@ -5,5 +7,22 @@
  * @author Marcelo Heredia
  */
 public class TextEncoder {
-
+	/**
+	 * Counts the frequency each character from the string appears.
+	 * 
+	 * @param input String to count frequency of each character
+	 * @return a HashMap using the characters from input as keys and their
+	 *         respective frequencies as values
+	 */
+	public HashMap<Character, Integer> StringToFrequencyMap(String input) {
+		HashMap<Character, Integer> frequencyMap = new HashMap<Character, Integer>();
+		for (int i = 0; i < input.length(); i++) {
+			char c = input.charAt(i);
+			// Se existe estiver no mapa pega a frequencia atual, se nao comeca em 0
+			int frequency = frequencyMap.containsKey(c) ? frequencyMap.get(c) : 0;
+			// Adiciona no mapa somando a frequencia + 1
+			frequencyMap.put(c, frequency + 1);
+		}
+		return frequencyMap;
+	}
 }
